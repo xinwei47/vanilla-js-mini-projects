b12504ed78f9030968a35f0dc618c9567626314b
Jodi Wei <xinwei47@gmail.com>
2022-03-09 15:48:51 -0600

    added styling

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -1,7 +1,8 @@
 const searchInput = document.querySelector('.search-form__input');
 const searchBtn = document.querySelector('.search-form__btn');
-const shuffleBtn = document.querySelector('.shuffle-btn');
-const resultsContainer = document.querySelector('.results');
+const shuffleBtn = document.querySelector('.search-shuffle__btn');
+const resultsContainer = document.querySelector('.results-container');
+const results = document.querySelector('.results');
 const mealDetails = document.querySelector('.meal');
 
 const searchMeals = async (e) => {
@@ -12,23 +13,38 @@ const searchMeals = async (e) => {
     `http://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
   );
   const meals = res.data.meals;
-  displayResults(meals);
+  displayResults(term, meals);
 };
 
-const displayResults = (data) => {
+const displayResults = (term, data) => {
   // console.log(data);
+  resultsContainer.innerHTML = '';
+  mealDetails.innerHTML = '';
+
+  const resultsHeading = document.createElement('h2');
+  const results = document.createElement('ul');
+
+  resultsHeading.textContent = `Search results for "${term}"`;
+  results.classList.add('results');
+
+  resultsContainer.append(resultsHeading);
+  resultsContainer.append(results);
+
   data.map((item) => {
     const list = document.createElement('li');
     list.classList.add('results__item');
     list.innerHTML = `
-    <a href="#mealDetails">
+    <a href="#mealDetails" class="results__item-link">
         <div class="results__item-img-box">
             <img class="results__item-img" src="${item.strMealThumb}" alt="" />
+        </div>
+        <div class="results__item-info">
+            <p class="results__item-title">${item.strMeal}</p>
         </div>
     </a>
     `;
     list.addEventListener('click', () => displayRecipe(item));
-    resultsContainer.append(list);
+    results.append(list);
   });
 };
 
@@ -50,10 +66,11 @@ const displayRecipe = (meal) => {
   `;
 
   const ingredients = document.createElement('div');
+  ingredients.classList.add('meal__ingredients');
   mealDetails.append(ingredients);
   ingArr.map((ing) => {
     const ingBox = document.createElement('span');
-    ingBox.classList.add('ingredient');
+    ingBox.classList.add('meal__ingredient');
     ingBox.innerText = ing;
     ingredients.append(ingBox);
   });
@@ -74,7 +91,7 @@ const getRandomMeal = async () => {
     'https://www.themealdb.com/api/json/v1/1/random.php'
   );
   const meal = res.data.meals[0];
-  resultsContainer.innerHTML = '';
+  results.innerHTML = '';
   displayRecipe(meal);
 };
 
