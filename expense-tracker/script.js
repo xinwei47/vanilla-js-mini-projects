05460c7a2e2e31e1c7e5260bb9bd6bfb5c860854
xinwei47 <xinwei47@gmail.com>
2022-03-14 17:06:31 -0500

    fixed bugs

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -6,22 +6,31 @@ const incomeDisplay = document.querySelector('.summary__income');
 const expenseDisplay = document.querySelector('.summary__expense');
 const history = document.querySelector('.history__list');
 
+// Bugs:
+// 1. cannot added transactions with the same name
+//    solution: change data structure to an array of objects. add transaction IDs: remove transactions by locating IDs
+// 2. user can add empty transactions
+//    solution: validate user input values which cannot be empty strings
+
+const getLocalStorageData = () => {
+  return localStorage.getItem('transactions')
+    ? JSON.parse(localStorage.getItem('transactions'))
+    : [];
+};
+
 const getData = () => {
   history.innerHTML = '';
   let balance = 0,
     income = 0,
     expense = 0;
 
-  const data = localStorage.getItem('transactions')
-    ? JSON.parse(localStorage.getItem('transactions'))
-    : {};
-
-  for (let item in data) {
-    const value = data[item];
+  const data = getLocalStorageData();
+  data.forEach((item) => {
+    const value = item.amt;
     balance += value;
     value >= 0 ? (income += value) : (expense += value);
-    addToHistory(item, value);
-  }
+    addToHistory(item);
+  });
 
   balanceDisplay.innerText = `$${balance.toFixed(2)}`;
   incomeDisplay.innerText = `$${income.toFixed(2)}`;
@@ -30,34 +39,51 @@ const getData = () => {
 
 const postTransaction = (event) => {
   event.preventDefault();
-  const item = textInput.value;
-  const amt = parseInt(amtInput.value);
 
-  const data = JSON.parse(localStorage.getItem('transactions'));
-  data[item] = amt;
-  localStorage.setItem('transactions', JSON.stringify(data));
-  getData();
+  if (textInput.value.trim() === '' || amtInput.value.trim() === '') {
+    alert('Please enter valid transaction name and amount.');
+  } else {
+    const transaction = {
+      id: generateId(),
+      name: textInput.value,
+      amt: parseInt(amtInput.value),
+    };
+
+    const data = getLocalStorageData();
+    data.push(transaction);
+    localStorage.setItem('transactions', JSON.stringify(data));
+    getData();
+  }
+
+  textInput.value = '';
+  amtInput.value = '';
 };
 
-const addToHistory = (item, amt) => {
+const generateId = () => {
+  return Math.floor(Math.random() * 100000);
+};
+
+const addToHistory = (item) => {
+  const name = item.name;
+  const amt = item.amt;
   const list = document.createElement('li');
   list.classList.add('history__item');
   list.innerHTML = `
     <button type="button" class="history__item-btn hidden">X</button>
-    <p class="history__item-title">${item}</p>
+    <p class="history__item-title">${name}</p>
     <p class="history__item-amt" >${amt > 0 ? `+${amt}` : amt}</p>
     <div class="history__item-mark ${amt > 0 ? 'green' : 'red'}"></div>
   `;
   history.append(list);
 
   const removeItemBtn = list.querySelector('.history__item-btn');
-  removeItemBtn.addEventListener('click', () => removeItem(item));
+  removeItemBtn.addEventListener('click', () => removeItem(item.id));
 };
 
-const removeItem = (key) => {
+const removeItem = (id) => {
   const data = JSON.parse(localStorage.getItem('transactions'));
-  delete data[key];
-  localStorage.setItem('transactions', JSON.stringify(data));
+  const updatedTrans = data.filter((item) => item.id !== id);
+  localStorage.setItem('transactions', JSON.stringify(updatedTrans));
   getData();
 };
 
